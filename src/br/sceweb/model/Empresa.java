5dbd481b8e5614a6f078256923480ebf26a5d460
Luiz Henrique Ferreira <luizferreira@gmail.com>
2017-09-28 21:48:13 -0300

    segunda versão (adicionado exclui no CT01 e EmpresaDAO)

--- src/br/sceweb/model/Empresa.java
+++ src/br/sceweb/model/Empresa.java
@@ -1,5 +1,54 @@
 package br.sceweb.model;
 
 public class Empresa {
+	String cnpj;
+	String nomeDaEmpresa;
+	String nomeFantasia;
+	String endereco;
+	String telefone;
 
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getNomeDaEmpresa() {
+		return nomeDaEmpresa;
+	}
+
+	public String setNomeDaEmpresa(String nomeDaEmpresa) {
+		String msg = "";
+		if (nomeDaEmpresa.equals("")) {
+			msg = "Nome da empresa inválido!";
+		} else
+			this.nomeDaEmpresa = nomeDaEmpresa;
+		return msg;
+	}
+
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
 }
--- teste/br/sceweb/teste_unitario/UC01CadastrarEmpresa.java
+++ teste/br/sceweb/teste_unitario/UC01CadastrarEmpresa.java
@@ -2,6 +2,7 @@ package br.sceweb.teste_unitario;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -17,15 +18,45 @@ public class UC01CadastrarEmpresa {
 	public static void setUpBeforeClass() throws Exception {
 		empresa = new Empresa();
 		empresaDAO = new EmpresaDAO();
+		empresa.setNomeDaEmpresa("empresa x");
+		empresa.setCnpj("89424232000180");
+		empresa.setNomeFantasia("empresa x");
+		empresa.setEndereco("rua taquari");
+		empresa.setTelefone("2222");
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 	}
 
+	/**
+	 * estabelece as pre-condicoes antes da execucao de cada teste
+	 * 
+	 * @throws Exception
+	 */
+	@After
+	public void excluiEmpresa() throws Exception {
+		empresaDAO.exclui("89424232000180");
+	}
+
+	/**
+	 * verifica o comportamento do sistema na inclusao de um cnpj valido
+	 */
 	@Test
-	public void CT01UC01Cadastrar_empresa_com_sucesso() {
-		assertEquals(1,empresaDAO.adiciona(empresa));
+	public void CT01UC01CadastrarEmpresa_com_sucesso() {
+		assertEquals(1, empresaDAO.adiciona(empresa));
+	}
+
+	/*
+	 * verifica a validação dos dados para none da empresa inválido
+	 */
+	@Test
+	public void CT02UC01CadastrarEmpresa_valida_nome() {
+		try {
+			empresa.setNomeDaEmpresa("");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Nome da empresa inválido!", e.getMessage());
+		}
 	}
 
 }
