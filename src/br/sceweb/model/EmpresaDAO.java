f966361f6148c08811e49520fed986fcddadd083
Luiz Henrique Ferreira <luizferreira@gmail.com>
2017-10-05 22:00:45 -0300

    Configuração 5 - Inclusão dos CT
    Inclusão dos casos de teste para cobertura do critério classes de
    equivalência

--- src/br/sceweb/model/EmpresaDAO.java
+++ src/br/sceweb/model/EmpresaDAO.java
@@ -1,6 +1,9 @@
 package br.sceweb.model;
 
 import java.sql.SQLException;
+
+import javax.sound.midi.Soundbank;
+
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
 import br.sceweb.servico.FabricaDeConexoes;
@@ -11,7 +14,7 @@ public class EmpresaDAO {
 		int codigoRetorno = 0;
 		try (Connection conn = new FabricaDeConexoes().getConnection()) {
 			ps = (PreparedStatement) conn.prepareStatement(
-					"insert into empresa (cnpj, nomeDaEmpresa, nomeFantasia, endereco, telefone) values(?,?,?,?,?)");
+			"insert into empresa (cnpj, nomeDaEmpresa, nomeFantasia, endereco, telefone) values(?,?,?,?,?)");
 			ps.setString(1, empresa.getCnpj());
 			ps.setString(2, empresa.getNomeDaEmpresa());
 			ps.setString(3, empresa.getNomeFantasia());
@@ -21,7 +24,7 @@ public class EmpresaDAO {
 			ps.close();
 
 		} catch (SQLException e) {
-			throw new RuntimeException(e);
+			System.out.println("Erro = " + e.getMessage());
 		}
 		return codigoRetorno;
 	}
--- teste/br/sceweb/teste_unitario/UC01CadastrarEmpresa.java
+++ teste/br/sceweb/teste_unitario/UC01CadastrarEmpresa.java
@@ -5,10 +5,15 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import br.sceweb.model.Empresa;
 import br.sceweb.model.EmpresaDAO;
+
+/** Este caso de teste ......
+ * @author Luiz Ferreira
+ */
 
 public class UC01CadastrarEmpresa {
 	public static Empresa empresa;
@@ -30,28 +35,47 @@ public class UC01CadastrarEmpresa {
 	}
 
 	/**
-	 * estabelece as pre-condicoes antes da execucao de cada teste
-	 * 
+	 * Estabelece as pré-condições antes da execucao de cada teste
 	 * @throws Exception
 	 */
 	@After
 	public void excluiEmpresa() throws Exception {
 		empresaDAO.exclui("89424232000180");
 	}
-
 	/**
-	 * verifica o comportamento do sistema na inclusao de um cnpj valido
+	 * ID - CT01UC01CadastrarEmpresa_com_sucesso
+	 * Objetivo - verificar o comportamento do sistema na inclusao de um cnpj valido
+	 * Pré-condição - O cnpj 89424232000180 nao estar cadastrado
 	 */
 	@Test
 	public void CT01UC01CadastrarEmpresa_com_sucesso() {
 		assertEquals(1, empresaDAO.adiciona(empresa));
 	}
-
-	/*
-	 * verifica a validação dos dados para none da empresa inválido
+	/**
+	 * ID - CT02UC01CadastrarEmpresa_cnpj_invalido
+	 * Objetivo - verificar o comportamento do sistema na inclusao de um cnpj invalido
+	 * Pré-condição - Usar um CNPJ invalido, por exemplo 2345678
 	 */
 	@Test
-	public void CT02UC01CadastrarEmpresa_valida_nome() {
+	public void CT02UC01CadastrarEmpresa_cnpj_invalido() {
+		assertEquals("CNPJ invalido.", empresa.setCnpj("2345678"));
+	}
+	/**
+	 * ID - CT03UC01CadastrarEmpresa_cnpj_ja_cadastrado
+	 * Objetivo - verificar o comportamento do sistema na inclusao de um cnpj ja cadastrado
+	 * Pré-condição - O CNPJ 89424232000180 já estar cadastrado
+	 */
+	@Test
+	public void CT03UC01CadastrarEmpresa_cnpj_ja_cadastrado() {
+		empresaDAO.adiciona(empresa);
+		assertEquals(0, empresaDAO.adiciona(empresa));
+	}
+	/**
+	 * ID - CT04UC01CadastrarEmpresa_valida_nome
+	 * Objetivo - verificar a validação dos dados para none da empresa inválido
+	 */
+	@Test
+	public void CT04UC01CadastrarEmpresa_valida_nome() {
 		try {
 			empresa.setNomeDaEmpresa("");
 		} catch (IllegalArgumentException e) {
